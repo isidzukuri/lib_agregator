5a60286fc22bca86e0ceca70742b40264d765daa
Isidzukuri <rostyslav@innocode.no>
2017-03-13 22:00:56 +0200

    select chtyvo

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -3,7 +3,7 @@ class Api::SearchController < ApplicationController
   def index
     if params[:word].present?
       books = Book.search_by_title(params[:word], @per_page, @offset)
-      books = books.select{|b| b.domain != 'yakaboo.ua'}
+      books = books.select{|b| b.domain == 'chtyvo.org.ua'}
       render json: books, each_serializer: BookInListSerializer
     end
 
