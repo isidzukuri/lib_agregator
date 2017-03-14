53c8a29a3612adefa5619073ee15c84a24b69ca0
Isidzukuri <rostyslav@innocode.no>
2017-03-14 21:38:55 +0200

    namespace

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -4,7 +4,7 @@ class Api::SearchController < ApplicationController
     if params[:word].present?
       books = Book.search_by_title(params[:word], @per_page, @offset)
       books = books.select{|b| b.domain == 'chtyvo.org.ua'}
-      render json: books, each_serializer: BookInListSerializer
+      render json: books, each_serializer: Api::BookInListSerializer
     end
 
   end
