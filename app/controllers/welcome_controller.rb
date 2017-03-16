86fe5d04f5a60f8c3d2f9eeda13051b8592c8ccd
Isidzukuri <rostyslav@innocode.no>
2017-03-16 22:28:25 +0200

    recomended paper, normal footer position, form for all pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
+    # 'shchighol', '1_filosofiia-svobodi', 'liudina-v-poshukakh-spravzhn-ogho-siensu-psikhologh-u-kontstabori', 'moie-zhittia-ta-robota', 'orighinali-iak-nonkonformisti-rukhaiut-svit', 'rework-tsia-knigha-pierieviernie-vash-poghliad-na-biznies', 'ilon-mask-tesla-spacex-i-shliakh-u-fantastichnie-maibutnie', 'chomu-natsiyi-zaniepadaiut-pokhodzhiennia-vladi-baghatstva-i-bidnosti', 'atlant-rozpraviv-pliechi-kompliekt-iz-3-knigh'
+
   def index
+    @paper_books = Book.where(id: [114629, 78556, 264418, 78153, 85199, 65867, 101084, 87251, 101015]).select(:title, :seo, :cover).order(id: :desc)
   end
 
 end
