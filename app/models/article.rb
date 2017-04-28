a62dd1832ee9be8ac9cd7c9a26dbb03a405fc39d
Isidzukuri <rostyslav@innocode.no>
2017-04-28 09:31:49 +0300

    articles crud

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,11 @@ class Article < ActiveRecord::Base
   include SeoName
   belongs_to :user
 
+  mount_uploader :cover, CoverUploader
+
   validates :title, presence: true
+
+  def thumb
+    cover ? cover.thumb.url : nil
+  end
 end
--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,8 +1,8 @@
-class ArticlePosterUploader < CarrierWave::Uploader::Base
+class CoverUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -11,13 +11,11 @@ class ArticlePosterUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
   end
 
    version :thumb do
-    process convert: JPG
-    process resize_to_fill: [300, -1]
-    process quality: 60
+    process resize_to_fill: [400, -1]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
