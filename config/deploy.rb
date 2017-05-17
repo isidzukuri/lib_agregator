df26e2fc81f8c7d05098773609b5769681c1dcda
Isidzukuri <rostyslav@innocode.no>
2017-05-18 02:03:44 +0300

    css

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,6 @@ set :application, "lib_agregator"
 set :repo_url, "https://github.com/isidzukuri/lib_agregator.git"
 set :deploy_to, '/var/www/lib_agregator'
 
+append :linked_dirs, 'public/uploads'
 
 append :linked_files, "config/database.yml", "config/secrets.yml", "public/sd-feed-269.xml"
