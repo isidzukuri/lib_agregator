b1610478ce31d1bceee0f0c692b9cd6c88ff1d74
Isidzukuri <rostyslav@innocode.no>
2017-05-24 09:12:12 +0300

    homepage content

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   # 'shchighol', '1_filosofiia-svobodi', 'liudina-v-poshukakh-spravzhn-ogho-siensu-psikhologh-u-kontstabori', 'moie-zhittia-ta-robota', 'orighinali-iak-nonkonformisti-rukhaiut-svit', 'rework-tsia-knigha-pierieviernie-vash-poghliad-na-biznies', 'ilon-mask-tesla-spacex-i-shliakh-u-fantastichnie-maibutnie', 'chomu-natsiyi-zaniepadaiut-pokhodzhiennia-vladi-baghatstva-i-bidnosti', 'atlant-rozpraviv-pliechi-kompliekt-iz-3-knigh'
 
   def index
-    @paper_books = Book.where(id: [114_629, 78_556, 264_418, 78_153, 85_199, 65_867, 101_084, 87_251, 101_015]).select(:title, :seo, :cover).order(id: :desc)
+    @paper_books = []#Book.where(id: [114_629, 78_556, 264_418, 78_153, 85_199, 65_867, 101_084, 87_251, 101_015]).select(:title, :seo, :cover).order(id: :desc)
     @free_books = free_books
     @lists = lists()
     @articles = articles()
@@ -13,7 +13,7 @@ class WelcomeController < ApplicationController
   def free_books
     items = $cache.read('free_books')
     unless items
-      items = Book.where(paper: nil).where.not(cover: nil).order('RAND()').limit(12)
+      items = Book.where(paper: nil).where.not(cover: nil).order('RAND()').limit(20)
       $cache.write('free_books', items, expires_in: 1.day)
     end
     items
