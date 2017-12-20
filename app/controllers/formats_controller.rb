d0c45bd223423fabcf8b82ef8364fb09fab3c17b
isidzukuri <axesigon@gmail.com>
2017-12-20 12:42:40 +0200

    Update formats_controller.rb

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -6,6 +6,6 @@ class FormatsController < ApplicationController
   def show
     # model_method = "find_by_#{params[:key]}".to_sym 
     redirect_to '/formats' unless $book_formats.include?(params[:key])
-    @items = Book.where.not(params[:key] => nil).includes(:authors).paginate(page: params[:page], per_page: @per_page)
+    @items = Book.where.not(params[:key] => nil).paginate(page: params[:page], per_page: @per_page).includes(:authors)
   end
 end
