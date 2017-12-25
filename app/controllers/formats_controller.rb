c0c899220ada5c87117edf01e509aa4eae3346de
isidzukuri <axesigon@gmail.com>
2017-12-25 13:37:03 +0200

    Update formats_controller.rb

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -6,6 +6,11 @@ class FormatsController < ApplicationController
   def show
     # model_method = "find_by_#{params[:key]}".to_sym 
     redirect_to '/formats' unless $book_formats.include?(params[:key])
-    @items = Book.select($book_required_fields).where.not(params[:key] => nil).paginate(page: params[:page], per_page: @per_page).includes(:authors)
+    cache_key = "fb_#{params[:key]}_#{params[:page]}" 
+    @items = $cache.read(cache_key)
+    unless items
+      @items = Book.select($book_required_fields).where.not(params[:key] => nil).paginate(page: params[:page], per_page: @per_page).includes(:authors)
+      $cache.write(cache_key, @items, expires_in: 1.day)
+    end
   end
 end
