0e2960d1e424aa7ed5f15d60b2b8a134cd047143
isidzukuri <axesigon@gmail.com>
2017-12-25 13:37:29 +0200

    Update formats_controller.rb

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -8,7 +8,7 @@ class FormatsController < ApplicationController
     redirect_to '/formats' unless $book_formats.include?(params[:key])
     cache_key = "fb_#{params[:key]}_#{params[:page]}" 
     @items = $cache.read(cache_key)
-    unless items
+    unless @items
       @items = Book.select($book_required_fields).where.not(params[:key] => nil).paginate(page: params[:page], per_page: @per_page).includes(:authors)
       $cache.write(cache_key, @items, expires_in: 1.day)
     end
