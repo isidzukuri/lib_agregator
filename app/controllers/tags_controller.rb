76b5bf44a2813c51c8c62dba7b9d17644f4da066
isidzukuri <axesigon@gmail.com>
2017-12-25 13:40:32 +0200

    Update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,11 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by_seo(params[:key])
-    @items = @tag.books.select($book_required_fields).includes(:authors).paginate(page: params[:page], per_page: @per_page)
+    cache_key = "tag_#{params[:key]}_#{params[:page]}" 
+    @items = $cache.read(cache_key)
+    unless @items
+      @items = @tag.books.select($book_required_fields).includes(:authors).paginate(page: params[:page], per_page: @per_page)
+      $cache.write(cache_key, @items, expires_in: 1.day)
+    end
   end
 end
