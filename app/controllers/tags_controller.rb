c179666b5bcb78b08bf935c24f42ef99970e6286
Isidzukuri <rostyslav@innocode.no>
2018-01-23 10:42:47 +0200

    tags paginate

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,20 +2,23 @@ class TagsController < ApplicationController
   caches_action :index, expires_in: 12.hour
 
   def index
-    @items = $cache.read('tags')
-    unless @items
-      @items = Tag.order(:title).all
-      $cache.write('tags', @items, expires_in: 5.day)
+    @all_items = cached('tags') do
+      Tag.order(:title).all
+    end
+    
+    @per_page = 300
+    cache_key = "tags_#{params[:page]}" 
+    @items = cached(cache_key, 30.day) do
+      Tag.order(:title).paginate(page: params[:page], per_page: @per_page).all
     end
   end
 
   def show
     @tag = Tag.find_by_seo(params[:key])
     cache_key = "tag_#{params[:key]}_#{params[:page]}" 
-    @items = $cache.read(cache_key)
-    unless @items
-      @items = @tag.books.select($book_required_fields).includes(:authors).paginate(page: params[:page], per_page: @per_page)
-      $cache.write(cache_key, @items, expires_in: 30.day)
+    @items = cached(cache_key) do
+      @tag.books.select($book_required_fields).includes(:authors).paginate(page: params[:page], per_page: @per_page)
     end
   end
+
 end
