89709d25ea88ddea9dac1f65e9f5a88ce56702d3
Isidzukuri <rostyslav@innocode.no>
2018-02-22 21:39:27 +0200

    book lists do not show copies

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module LibAgreagator
   class Application < Rails::Application
     $book_formats = ['txt', 'rtf', 'doc', 'pdf', 'fb2', 'epub', 'mobi', 'djvu', 'paper'] 
-    $book_required_fields = ['id', 'title', 'seo'] + $book_formats
+    $book_required_fields = ['id', 'title', 'seo', 'is_copy'] + $book_formats
     $cache = ActiveSupport::Cache::MemCacheStore.new
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
