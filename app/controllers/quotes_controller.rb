860e24e513965cd54cfe04bacf91ed749937d296
Isidzukuri <rostyslav@innocode.no>
2018-04-25 03:04:02 +0300

    cache

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,10 @@
 class QuotesController < ApplicationController
+  caches_action :index, expires_in: 12.hour, :cache_path => Proc.new { |c| c.params }
+
+
   def index
     @per_page = 15
-    cache_key = "quotes_#{params[:page]}" 
+    # cache_key = "quotes_#{params[:page]}" 
     # @items = cached(cache_key) do
       @items = Quote.order(id: :desc).paginate(page: params[:page], per_page: @per_page).includes(book: :authors).all
     # end
