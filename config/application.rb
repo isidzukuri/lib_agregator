891ff52217c0abd355c45cd0980a49095b7021a7
Isidzukuri <axesigon@gmail.com>
2019-03-30 23:33:01 +0200

    update rails to 523

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module LibAgreagator
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths += Dir["#{Rails.root}/app/serializers/**/"]
     config.autoload_paths += Dir["#{Rails.root}/app/services/**"]
