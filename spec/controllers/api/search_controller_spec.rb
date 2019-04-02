fa151fa211ee5ae2528bca3caa69a99e61629c51
Isidzukuri <axesigon@gmail.com>
2019-04-02 09:19:32 +0300

    rubocop

--- spec/controllers/api/search_controller_spec.rb
+++ spec/controllers/api/search_controller_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe Api::SearchController, type: :controller do
   # let!(:list) { create(:list, status: 'published', books: [create(:book), create(:book)]) }
   # let!(:unpublished_list) { create(:list, status: 'unpublished') }
 
-  let!(:book){ create(:book, title: 'Some title', domain:'chtyvo.org.ua', source: '/books/some_id')}
-  let!(:book_paper){ create(:book, title: 'Some paper title', source: '/books/paper_id')}
+  let!(:book) { create(:book, title: 'Some title', domain: 'chtyvo.org.ua', source: '/books/some_id') }
+  let!(:book_paper) { create(:book, title: 'Some paper title', source: '/books/paper_id') }
 
-  let(:parsed_body){ JSON.parse(response.body)}
+  let(:parsed_body) { JSON.parse(response.body) }
 
   context 'GET /api/search' do
     it '' do
@@ -18,11 +18,10 @@ RSpec.describe Api::SearchController, type: :controller do
 
       expect(parsed_body[0]['title']).to eq(book.title)
       expect(parsed_body[0]['author_title']).to eq(book.authors.first.full_name)
-      expect(parsed_body[0]['source']).to eq("http://chtyvo.org.ua/books/some_id")
-      expect(parsed_body[0].keys).to eq(["id", "title", "source", "author_title", "cover", "paper"])
+      expect(parsed_body[0]['source']).to eq('http://chtyvo.org.ua/books/some_id')
+      expect(parsed_body[0].keys).to eq(%w(id title source author_title cover paper))
     end
   end
-
 
   context 'GET /api/paper' do
     it '' do
@@ -30,14 +29,13 @@ RSpec.describe Api::SearchController, type: :controller do
 
       get :paper, params: { word: 'some' }
 
-      expect(parsed_body[0].keys).to eq(["id", "title", "source", "author_title", "cover", "paper"])
+      expect(parsed_body[0].keys).to eq(%w(id title source author_title cover paper))
       expect(parsed_body.length).to eq(2)
       expect(parsed_body[0]['title']).to eq(book.title)
       expect(parsed_body[0]['author_title']).to eq(book.authors.first.full_name)
-      expect(parsed_body[0]['source']).to eq("http://chtyvo.org.ua/books/some_id")
+      expect(parsed_body[0]['source']).to eq('http://chtyvo.org.ua/books/some_id')
     end
   end
-
 
   # api_search GET    /api/search(.:format)    api/search#index {:format=>"json"}
   # api_paper GET    /api/paper(.:format)
