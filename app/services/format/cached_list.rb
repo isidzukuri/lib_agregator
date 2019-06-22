3a9640a8c320a6d638e4ee07509bfe167f90fcfb
Isidzukuri <axesigon@gmail.com>
2019-06-23 00:13:57 +0300

    remove logic from formats controller

---
+++ app/services/format/cached_list.rb
@@ -0,0 +1,27 @@
+class Format::CachedList
+  def initialize(params)
+    @params = params
+  end
+
+  attr_accessor :params
+
+  def call
+    Base::CachedData.call(cache_key, 30.day) do
+      Book.select(Book::VIEW_ATTRIBUTES).where.not(params[:key] => nil).paginate(page: page, per_page: limit).includes(:authors)
+    end
+  end
+
+  private
+
+  def cache_key
+    "fb_#{params[:id]}_#{params[:page]}"
+  end
+
+  def limit
+    params[:limit] || 100
+  end
+
+  def page
+    params[:page] || 1
+  end
+end
