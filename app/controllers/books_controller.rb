b7d55e4947b5c6a7a82b36e73f32d45bc9d13808
Isidzukuri <axesigon@gmail.com>
2019-06-23 00:21:53 +0300

    remove logic from books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,25 +5,12 @@ class BooksController < ApplicationController
     redirect_to(:root, status: 410) unless @book
     redirect_to(:root, status: 410) if ActiveModel::Type::Boolean.new.cast(@book['hide'])
 
-    @read_also = read_also(@book)
+    @read_also = Book::ReadAlso.new(book: @book).call
   end
 
   def autocomplete_with_seo
     items = Book::AutocompleteWithSeoQuery.new(word: params[:term]).call
 
     render json: items, each_serializer: BookAutocompleteSerializer
-  end
-
-  private
-
-  def read_also book
-    tags_ids = book['tags_ids']&.split(',')
-    return nil unless tags_ids
-      
-    tags_ids = tags_ids.map(&:to_i).sort
-    cache_key = "also_#{tags_ids[0..3].join}_2" 
-    Base::CachedData.call(cache_key, 10.day) do
-      Book::ReadAlsoQuery.new(book_id: book[:id], tags_ids: tags_ids, language: book[:language]).call
-    end
   end
 end
