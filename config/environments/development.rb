c99c67db9a10d5746f31ec1303de2bbbac877caa
isidzukuri <axesigon@gmail.com>
2019-10-31 20:00:19 +0200

    key not found ciphers fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ciphers] = OpenSSL::SSL::SSLContext.new.ciphers
 end
